f83b5dc514d4eb5662f0477d76847e66febf28a1
Cynthia <cturner@ltu.edu>
2019-02-27 15:49:04 -0500

    initial commit

---
+++ src/main/java/co/grandcircus/MovieAPi/MovieAPiApplication.java
@@ -0,0 +1,13 @@
+package co.grandcircus.MovieAPi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieAPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieAPiApplication.class, args);
+	}
+
+}
